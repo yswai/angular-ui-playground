1f96593c85008188bb49cd57597c5d50e9496c41
ysw <yswai1986@gmail.com>
2016-01-30 21:43:25 +0800

    fixed a lot issues

--- src/app/layout/edit-team.controller.js
+++ src/app/layout/edit-team.controller.js
@@ -1,18 +1,19 @@
 (function () {
     'use strict';
 
-    angular.module('eliteAdmin').controller('DeleteConfirmCtrl', DeleteConfirmCtrl);
+    angular.module('eliteAdmin').controller('EditTeamCtrl', EditTeamCtrl);
 
-    DeleteConfirmCtrl.$inject = ['$uibModalInstance', 'properties'];
+    EditTeamCtrl.$inject = ['$uibModalInstance', 'properties', 'team'];
 
-    function DeleteConfirmCtrl($uibModalInstance, properties) {
+    function EditTeamCtrl($uibModalInstance, properties, team) {
         var vm = this;
         vm.properties = properties;
         vm.ok = ok;
         vm.cancel = cancel;
+        vm.team = team;
 
         function ok() {
-            $uibModalInstance.close(true);
+            $uibModalInstance.close(vm.team);
         };
 
         function cancel() {
--- src/app/layout/league-shell.controller.js
+++ src/app/layout/league-shell.controller.js
@@ -3,9 +3,9 @@
 
     angular.module('eliteAdmin').controller('LeagueShellCtrl', LeagueShellCtrl);
 
-    LeagueShellCtrl.$inject = ['$routeParams', 'initialData', '$location'];
+    LeagueShellCtrl.$inject = ['$uibModal', '$routeParams', 'initialData', '$location'];
 
-    function LeagueShellCtrl($routeParams, initialData, $location) {
+    function LeagueShellCtrl($uibModal, $routeParams, initialData, $location) {
         /* jshint validthis:true */
         var vm = this;
         vm.leagueId = $routeParams.leagueId;
@@ -17,9 +17,12 @@
         ];
         vm.toggleCollapseAll = toggleCollapseAll;
         vm.collapseAll = false;
-        vm.groupedTeams = {};
+        vm.groupedTeams = [];
         vm.teams = initialData;
         vm.go = go;
+        vm.addNewTeam = addNewTeam;
+        vm.editTeam = editTeam;
+        vm.deleteTeam = deleteTeam;
 
         activate();
 
@@ -29,24 +32,102 @@
 
         function activate() {
             _.first(vm.tabs).active = true;
+            initializeGroup();
+        }
+
+        function initializeGroup() {
+            var openedGroups = [];
+            _.each(vm.groupedTeams, function(group) {
+                if (group.isOpen) openedGroups.push(group.groupName);
+            });
             vm.groupedTeams = _.chain(vm.teams)
                 .filter(function(t) {
                     return t.leagueId === parseInt($routeParams.leagueId);
                 })
                 .groupBy('divisionName')
-                .map(function(v, k) {
+                .map(function(teams, groupName) {
                     return {
-                        groupName: k,
-                        isOpen: false,
-                        teams: v
+                        groupName: groupName,
+                        isOpen: _.contains(openedGroups, groupName),
+                        teams: teams
                     };
                 }).value();
-            _.first(vm.groupedTeams).isOpen = true;
         }
 
         function toggleCollapseAll(isCollapsed) {
             _.each(vm.groupedTeams, function(league) {
                 league.isOpen = !isCollapsed;
+            });
+        }
+
+        function addNewTeam() {
+            var modal = $uibModal.open({
+                templateUrl: 'app/layout/edit-team.html',
+                controller: 'EditTeamCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    properties: {
+                        title: 'Add new team',
+                        button: [
+                            'Save', 'Cancel'
+                        ]
+                    },
+                    team: {}
+                }
+            });
+            modal.result.then(function(newTeam) {
+                newTeam.leagueId = parseInt($routeParams.leagueId);
+                newTeam.id = _.uniqueId('team_');
+                vm.teams.push(newTeam);
+                initializeGroup();
+            });
+        }
+
+        function editTeam(team) {
+            var modal = $uibModal.open({
+                templateUrl: 'app/layout/edit-team.html',
+                controller: 'EditTeamCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    properties: {
+                        title: 'Edit team "' + team.name + '"',
+                        button: [
+                            'Save', 'Cancel'
+                        ]
+                    },
+                    team: function() {
+                        return angular.copy(team);
+                    }
+                }
+            });
+            modal.result.then(function(editedTeam) {
+                _.each(vm.teams, function(team) {
+                    if (team.id === editedTeam.id) {
+                        _.assign(team, editedTeam);
+                    }
+                });
+                initializeGroup();
+            });
+        }
+
+        function deleteTeam(team) {
+            var modal = $uibModal.open({
+                templateUrl: 'app/leagues/delete-confirm.html',
+                controller: 'DeleteConfirmCtrl',
+                controllerAs: 'vm',
+                resolve: {
+                    properties: {
+                        title: 'Delete confirmation',
+                        message: 'Are you sure you want to delete team "' + team.name + '"?',
+                        button: [
+                            'Ok', 'Cancel'
+                        ]
+                    }
+                }
+            });
+            modal.result.then(function() {
+                _.remove(vm.teams, { id: team.id });
+                initializeGroup();
             });
         }
 
